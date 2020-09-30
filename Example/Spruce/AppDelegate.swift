0584bfba359151e6dcf1199dacf1c2f9f1690cc1
Zack Sheppard <zack@zacksheppard.com>
2020-09-30 14:02:27 -0400

    Fix compilation of the example project

--- Example/Spruce/AppDelegate.swift
+++ Example/Spruce/AppDelegate.swift
@@ -31,7 +31,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         let sortFunctionController = UIStoryboard(name: "SortFunctionTestViewController", bundle: Bundle.main).instantiateViewController(withIdentifier: "SortFunctionTestViewController")
         sortFunctionController.tabBarItem = UITabBarItem(title: "Sort Functions", image: UIImage(named: "ic_sortFunction"), tag: 0)
--- Example/Spruce/SortFunctionTestViewController.swift
+++ Example/Spruce/SortFunctionTestViewController.swift
@@ -130,13 +130,13 @@ class SortFunctionTestViewController: UIViewController {
         functionTextField.inputAccessoryView = toolbar
     }
 
-    func reloadSortView() {
+    @objc func reloadSortView() {
         
         animationController?.view.removeFromSuperview()
-        animationController?.removeFromParentViewController()
+        animationController?.removeFromParent()
         
         let testController = self.animationControllerForCurrentSettings()
-        self.addChildViewController(testController)
+        self.addChild(testController)
         self.sortView.addSubview(testController.view)
         testController.view.frame = sortView.bounds
         testController.setup()
@@ -342,7 +342,7 @@ extension SortFunctionTestViewController: UIPickerViewDataSource {
         return availableFunctions.count
     }
     
-    func sortFunctionSelected() {
+    @objc func sortFunctionSelected() {
         guard let row = pickerView?.selectedRow(inComponent: 0) else {
             return
         }
@@ -352,9 +352,9 @@ extension SortFunctionTestViewController: UIPickerViewDataSource {
         reloadSortView()
     }
     
-    func cancelPicker() {
+    @objc func cancelPicker() {
         self.functionTextField.endEditing(true)
-        guard let row = availableFunctions.index(of: settings.function) else {
+        guard let row = availableFunctions.firstIndex(of: settings.function) else {
             return
         }
         pickerView?.selectRow(row, inComponent: 0, animated: true)
