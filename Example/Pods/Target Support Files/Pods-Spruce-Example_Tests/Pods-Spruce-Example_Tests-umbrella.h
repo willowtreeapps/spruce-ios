89d01a3656f1fd46d73b91ea10202fa8c31f0f20
Zack Sheppard <zack@zacksheppard.com>
2020-09-30 14:00:35 -0400

    Run pod install on the example project

--- Example/Pods/Target Support Files/Pods-Spruce-Example_Tests/Pods-Spruce-Example_Tests-umbrella.h
+++ Example/Pods/Target Support Files/Pods-Spruce-Example_Tests/Pods-Spruce-Example_Tests-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_Spruce_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Spruce_ExampleVersionString[];
+FOUNDATION_EXPORT double Pods_Spruce_Example_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Spruce_Example_TestsVersionString[];
 
